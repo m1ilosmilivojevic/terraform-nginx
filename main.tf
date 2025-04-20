e87bc1cc92e5f5ad9749caff697db66db7e554fc
m1ilosmilivojevic <107323202+m1ilosmilivojevic@users.noreply.github.com>
2025-04-20 22:14:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,27 +3,54 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "ubuntu_ec2" {
-  ami                    = "ami-0c1ac8a41498c1a9c" # Ubuntu AMI for eu-north-1
-  instance_type          = "t3.micro"
-  key_name               = "miles-key2"  # optional: if you want to SSH in
-  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
-  user_data              = file("init.sh")
+resource "aws_vpc" "tf_custom_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support  = true
+  enable_dns_hostnames = true
   tags = {
-    Name = "MilesEC2"
+    Name = "tf-custom-vpc"
   }
 }
 
-resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http"
-  description = "Allow SSH and HTTP"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.tf_custom_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-north-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
   }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.tf_custom_vpc.id
+  tags = {
+    Name = "main-gateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.tf_custom_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-route"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP traffic"
+  vpc_id      = aws_vpc.tf_custom_vpc.id
 
   ingress {
     from_port   = 80
@@ -38,9 +65,22 @@ resource "aws_security_group" "allow_ssh_http" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "allow_http"
+  }
 }
 
-resource "aws_eip" "miles_eip" {
-  instance = aws_instance.ubuntu_ec2.id
-  domain      = "vpc"
+resource "aws_instance" "web_provisioned" {
+  ami                    = "ami-0c1ac8a41498c1a9c" # Ubuntu 22.04 LTS in eu-north-1
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  key_name               =                         #put your key here
+  user_data              = file("init.sh")
+
+  tags = {
+    Name = "Provisioned Web EC2"
+  }
 }
