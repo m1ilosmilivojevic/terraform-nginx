888c28c600054d1a158911d5f0e772f432601fb0
m1ilosmilivojevic <107323202+m1ilosmilivojevic@users.noreply.github.com>
2025-04-20 22:14:57 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 apt update -y
 apt install -y nginx
-echo "<h1>Hello from Terraform EC2</h1>" > /var/www/html/index.html
+echo "<h1>Hello from Terraform</h1>" > /var/www/html/index.html
+systemctl enable nginx
+systemctl start nginx
